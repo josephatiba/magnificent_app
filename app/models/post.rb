3f9c243af75d525c94052cc49de0e7eb79a56e71
Joey Atiba <joey.atiba@gmail.com>
2015-01-07 16:10:55 -0800

    Adds Timestampts to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,19 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :title, type: String
   field :link, type: String
   field :description, type: String
   belongs_to :user
+
+  def date_published
+  	# technically we don't need self. because its an instance variable so it's implied
+  	self.created_at.localtime.strftime("%A, %B %-d, %Y at %l:%M %p")
+  end
+
+  def date_added
+    created_at.localtime.strftime("%-m/%-d/%Y | %l:%M %p")
+  end
+
 end
