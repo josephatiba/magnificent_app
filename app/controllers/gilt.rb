5050dfe91948525245275a63d9fc9c7b4d67f3ac
Joey Atiba <joey.atiba@gmail.com>
2015-03-29 20:18:11 -0700

    dynamically adds store name as a class to gilt api post divs

--- app/controllers/gilt.rb
+++ app/controllers/gilt.rb
@@ -15,9 +15,9 @@ class Gilt
 
   def gilt_search
 
-     gilt_men = HTTParty.get("https://#{API_HOST}/sales/active.json?apikey=#{@key}")
-     gilt_men_results = JSON.parse(gilt_men.body)
-     gilt_men_results["sales"]
+     gilt_all_sales = HTTParty.get("https://#{API_HOST}/sales/active.json?apikey=#{@key}")
+     gilt_all_sales_results = JSON.parse(gilt_all_sales.body)
+     gilt_all_sales_results["sales"]
 
 
      # gilt_women = HTTParty.get("https://#{API_HOST}/sales/women/active.json?apikey=#{@key}")
@@ -26,25 +26,25 @@ class Gilt
      # # gilt_search.push(gilt_men_results)
 
 
-     # array_of_sales_results_combined = []
+     array_of_sales_results_combined = []
 
-     # gilt_men_results["sales"].each do |item|
+     gilt_all_sales_results["sales"].each do |item|
      #  # initialize container array
-     #    container_of_sales = {
-     #      sale_name: "",
-     #      image_url: "",
-     #      sale_url: "", 
-     #      store: ""
-     #    }
-     #    # put in container array values
-     #    container_of_sales[:sale_name] = item["name"]
-     #    container_of_sales[:image_url] = item["image_urls"]["315x295"][0]["url"]
-     #    container_of_sales[:sale_url] = item["sale_url"]
-     #    container_of_sales[:store] = item["store"]
+        container_of_sales = {
+          sale_name: "",
+          image_url: "",
+          sale_url: "", 
+          store: ""
+        }
+        # put in container array values
+        container_of_sales[:sale_name] = item["name"]
+        container_of_sales[:image_url] = item["image_urls"]["315x295"][0]["url"]
+        container_of_sales[:sale_url] = item["sale_url"]
+        container_of_sales[:store] = item["store"]
 
-     #    # push container array to array of sales results
-     #    array_of_sales_results_combined.push(container_of_sales)
-     # end
+        # push container array to array of sales results
+        array_of_sales_results_combined.push(container_of_sales)
+     end
 
      # gilt_women_results["sales"].each do |item|
      #  # initialize container array
@@ -63,7 +63,7 @@ class Gilt
      #    array_of_sales_results_combined.push(container_of_sales)
      # end
 
-     # return array_of_sales_results_combined.shuffle
+     return array_of_sales_results_combined.shuffle
 
 
   end
